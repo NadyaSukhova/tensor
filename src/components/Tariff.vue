637f2f9a073145bf732afe55fd22b0e0b927c38d
NadyaSukhova <suhova1313@gmail.com>
2024-02-13 02:16:01 +0300

    fixed tariff button & popup message

--- src/components/Tariff.vue
+++ src/components/Tariff.vue
@@ -21,7 +21,8 @@
                             компонентов
                         </div>
                     </div>
-                    <button type="button" class="popup-button">Начать бесплатно</button>
+                    <button type="button" class="popup-button" @:click="show = true">Начать
+                        бесплатно</button>
                 </form>
             </div>
             <div class="variant">
@@ -37,37 +38,38 @@
                             и многостраничные сайты из готовых компонентов
                         </div>
                     </div>
-                    <button type="button" class="popup-button" @:click="show = true, console.log(show)">Начать
+                    <button type="button" class="popup-button" @:click="show = true">Начать
                         бесплатно</button>
                 </form>
             </div>
         </div>
     </div>
-
-    <div class="popup" v-if="show">
-        <div class="title">Оставить заявку</div>
-        <div class="desc">Менеджеры перезвонят или напишут в течение дня. Поможем определиться и протестировать продукт.
-            Без спама и навязчивости</div>
-        <div class="fields">
-            <div class="input-box">
-                <input placeholder="Email" type="text">
+    <div class="message">
+        <div class="popup" v-if="show">
+            <div class="title">Оставить заявку</div>
+            <div class="desc">Менеджеры перезвонят или напишут в течение дня. Поможем определиться и протестировать продукт.
+                Без спама и навязчивости</div>
+            <div class="fields">
+                <div class="input-box">
+                    <input placeholder="Email" type="text">
+                </div>
+                <hr>
+                <div class="input-box">
+                    <input placeholder="Иван Иванов" type="text">
+                </div>
+                <hr>
+                <div class="input-box">
+                    <input placeholder="Телефон" type="text">
+                </div>
+                <hr>
             </div>
-            <hr>
-            <div class="input-box">
-                <input placeholder="Иван Иванов" type="text">
-            </div>
-            <hr>
-            <div class="input-box">
-                <input placeholder="Телефон" type="text" >
-            </div>
-            <hr>
+            <button @:click="show = false, changeResult(), console.log(show)">Отправить</button>
+            <div class=""></div>
         </div>
-        <button @:click="show = false, changeResult(), console.log(show)">Отправить</button>
-        <div class=""></div>
-    </div>
-    <div class="popup" v-if="result">
-        <div class="title">Спасибо за заявку</div>
-        <div class="desc">Наши менеджеры свяжутся с вами в течении 5 минут</div>
+        <div class="popup" v-if="result">
+            <div class="title">Спасибо за заявку</div>
+            <div class="desc">Наши менеджеры свяжутся с вами в течении 5 минут</div>
+        </div>
     </div>
 </template>
 
